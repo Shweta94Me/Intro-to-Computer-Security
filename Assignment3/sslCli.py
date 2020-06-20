2eaf391db628490623bc67ab7bac9f54370f02be
Shweta94Me <mestryshweta30@gmail.com>
2020-06-20 17:22:56 -0400

    Code updated

--- Assignment3/sslCli.py
+++ Assignment3/sslCli.py
@@ -2,38 +2,52 @@ import socket
 import sys
 import ssl
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Invalid input. Please enter python program_name.py domain_name')
-        sys.exit()
-    else:
-        host = sys.argv[1]
+class sslCli:
+    def __init__(self,host,port):
+        self.host = host
+        self.port = port
+        self.s = None
+
+    def bindSocket(self):
         try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             print('Socket successfully created')
         except socket.error as err:
             print('Socket creation failed with ' + str(err))
+            sys.exit()
+        self.makeRequest()
+    
+    def makeRequest(self):
         try:
-            secure_soc = ssl.wrap_socket(s,
+            secure_soc = ssl.wrap_socket(self.s,
                                         server_side=False,
                                         ca_certs='smestry1.pem',
                                         cert_reqs=ssl.CERT_REQUIRED)
         except socket.error as err:
             print('SSL wrap failed ' + str(err))
             sys.exit()
-        port = 3000
         try:
-            secure_soc.connect((host, port))
+            secure_soc.connect((self.host, self.port))
         except:
-            print('No server found on ' + str(host) + ' at port ' + str(port))
+            print('No server found on ' + str(self.host) + ' at port ' + str(self.port))
             sys.exit()
         print('\nPlease enter User ID:\n')
         userID = (str(input())).strip()
         print('\nPlease enter Password:\n')
         password = (str(input())).strip()
         secure_soc.sendall(str.encode('\n'.join([userID,password])))
-        reply = str(secure_soc.recv(4096).decode('utf-8'))
+        reply = str(secure_soc.recv(1024).decode('utf-8'))
         print(reply.strip())
-        s.close()
+        self.s.close()
         secure_soc.close()
         print('Client closed')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Invalid input. Please enter python program_name.py domain_name')
+        sys.exit()
+    else:
+        host = sys.argv[1]
+        port = 3000
+        cli = sslCli(host,port)
+        cli.bindSocket()
--- Assignment3/sslServ.py
+++ Assignment3/sslServ.py
@@ -3,59 +3,76 @@ import ssl
 import sys
 import os 
 
-def readPassFile():
-    data = {}
-    if os.path.isfile('password'):
-        with open ('password', 'r') as fp:
-            for line in fp:
-                word = line.split()
-                data[word[0]] = word[1]
-            fp.close()
-    return data
+class sslServ:
+    def __init__(self,host,port):
+        self.host = host
+        self.port = port
+        self.s = None
+
+    def bindSocket(self):
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            print('Socket successfully created')
+        except socket.error as err:
+            print('Socket creation failed with ' + str(err))
+            sys.exit()
+        try:   
+            self.s.bind((self.host,self.port))
+            print('Socket is binded to port '+ str(self.port))
+        except socket.error as err:
+            print('Socket binding failed with ' + str(err))
+            sys.exit()
+        try:
+            self.s.listen(1)
+            print('Socket is listing')
+        except socket.error as err:
+            print('Socket listening failed with ' + str(err))
+            sys.exit()
+        self.getRequest()
     
-if __name__ == '__main__':
-    if len(sys.argv) != 1:
-        print('Invalid input. Please enter python program_name.py')
-        sys.exit()
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        print('Socket successfully created')
-    except socket.error as err:
-        print('Socket creation failed with ' + str(err))
-    port = 3000
-    s.bind(('127.0.0.1',port))
-    print('Socket is binded to port '+ str(port))
-    s.listen(1)
-    print('Socket is listing')
-    
-    client, addr = s.accept()
-    try:
-        secure_soc = ssl.wrap_socket(client, 
+    def readPassFile(self):
+        data = {}
+        if os.path.isfile('password'):
+            with open ('password', 'r') as fp:
+                for line in fp:
+                    word = line.split()
+                    data[word[0]] = word[1]
+                fp.close()
+        return data
+
+    def getRequest(self):
+        client, addr = self.s.accept()
+        try:
+            secure_soc = ssl.wrap_socket(client, 
                                      server_side=True, 
                                      certfile='smestry1.pem',
                                      keyfile='privkey.pem',
                                      ssl_version=ssl.PROTOCOL_TLSv1
                                      )
-    except socket.error as err:
-        print('SSL wrap failed ' + str(err))
-        sys.exit()
-    print('Got connection request from ' + str(addr))
-    userID, password = [str(i) for i in secure_soc.recv(1024).decode('utf-8').split('\n')]
-    print('UserID : ' + str(userID) + 'Password : ' + str(password))
-    data = readPassFile()
-    if userID in data:
-        if data[userID] == password:
-            result = 'correct ID and password'
+        except socket.error as err:
+            print('SSL wrap failed ' + str(err))
+            sys.exit()
+        userID, password = [str(i) for i in secure_soc.recv(1024).decode('utf-8').split('\n')]
+        data = self.readPassFile()
+        if userID in data:
+            if data[userID] == password:
+                result = 'correct ID and password'
+            else:
+                result = 'incorrect ID and password'
         else:
             result = 'incorrect ID and password'
-    else:
-        result = 'incorrect ID and password'
-    secure_soc.send(str.encode(result))
-    s.close()
-    secure_soc.close()
-    print('Server closed')
+        secure_soc.send(str.encode(result))
+        self.s.close()
+        secure_soc.close()
+        print('Server closed')
 
-    
-        
-    
+if __name__ == '__main__':
+    if len(sys.argv) != 1:
+        print('Invalid input. Please enter python program_name.py')
+        sys.exit()
+    else:
+        host = socket.gethostname()
+        port = 3000
+        serv = sslServ(host,port)
+        serv.bindSocket()
